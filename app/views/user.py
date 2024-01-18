530597013a963bed41a05c2c3fcf20ebecbc9c05
daehwi <kdh92417@gmail.com>
2024-01-19 00:09:14 +0900

    로그인 API, 로그아웃 API 작성

--- app/views/user.py
+++ app/views/user.py
@@ -3,7 +3,7 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from app.serializers.user import SignupSerializer
+from app.serializers.user import SignupSerializer, LoginSerializer, LogoutSerializer
 
 
 class SignupView(CreateAPIView):
@@ -19,3 +19,36 @@ class SignupView(CreateAPIView):
         self.perform_create(serializer)
 
         return Response({"message": "회원가입이 완료되었습니다."}, status=status.HTTP_201_CREATED)
+
+
+class LoginView(CreateAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        token = serializer.validated_data["token"]
+
+        login_response_data = {
+            "access_token": str(token.access_token),
+            "refresh_token": str(token),
+        }
+
+        return Response(login_response_data, status=status.HTTP_200_OK)
+
+
+class LogoutView(CreateAPIView):
+    serializer_class = LogoutSerializer
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
