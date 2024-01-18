a3ab8fc82f2782f016c0099c6ff44487a01be2d3
daehwi <kdh92417@gmail.com>
2024-01-19 01:23:54 +0900

    상품 초성필드 추가

--- app/models/product.py
+++ app/models/product.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from app.untils import convert_to_initial_letters
+
 
 class SizeType(models.TextChoices):
     small = ("small",)
@@ -14,8 +16,11 @@ class Product(models.Model):
     barcode = models.CharField("제목", max_length=100, unique=True)
     price = models.PositiveIntegerField("가격")
     cost = models.PositiveIntegerField("원가")
-    name = models.CharField("상품 이름", max_length=100, db_index=True)
     description = models.TextField("상품 설명")
+
+    name = models.CharField("상품 이름", max_length=100)
+    initial_consonant = models.CharField("상품 이름", max_length=100, null=True, blank=True)
+
     expiration_date = models.DateField("유통기한")
 
     size = models.CharField("사이즈", max_length=15, choices=SizeType.choices)
@@ -25,3 +30,7 @@ class Product(models.Model):
 
     class Meta:
         verbose_name = verbose_name_plural = "상품"
+
+    def save(self, *args, **kwargs):
+        self.initial_consonant = convert_to_initial_letters(self.name)
+        super().save(*args, **kwargs)
