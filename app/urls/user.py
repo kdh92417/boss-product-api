e69baa5615efd4e7d66f3b2d1fe31019c6f04077
daehwi <kdh92417@gmail.com>
2024-01-19 00:09:28 +0900

    로그인 API, 로그아웃 API 엔드포인트 작성

--- app/urls/user.py
+++ app/urls/user.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from app.views.user import SignupView
+from app.views.user import SignupView, LoginView, LogoutView
 
 urlpatterns = [
     path("user/signup", SignupView.as_view(), name="signup"),
+    path("user/login", LoginView.as_view(), name="login"),
+    path("user/logout", LogoutView.as_view(), name="logout"),
 ]
