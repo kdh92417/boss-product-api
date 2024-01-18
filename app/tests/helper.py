0f571f7b8341fc909aa7dfa4d63cb65cb3f808d4
daehwi <kdh92417@gmail.com>
2024-01-19 01:24:28 +0900

    Product TestCase 모듈 작성

--- app/tests/helper.py
+++ app/tests/helper.py
@@ -1,8 +1,10 @@
+import random
 from collections import OrderedDict
 
 from faker import Faker
 
-from app.models import User
+from app.models import User, Product
+from app.models.product import SizeType
 
 locales = OrderedDict(
     [
@@ -22,3 +24,40 @@ def create_fake_user(phone_number=None, password=None) -> (User, str):
     )
 
     return user, password
+
+
+def create_product(
+    category=None,
+    barcode=None,
+    price=None,
+    cost=None,
+    name=None,
+    description=None,
+    expiration_date=None,
+    size=None,
+    user: User = None,
+):
+    if not user:
+        user, _ = create_fake_user()
+
+    product = Product.objects.create(
+        category=category or fake.name(),
+        barcode=barcode or fake.ssn(),
+        price=price or random.randint(1, 100) * 1000,
+        cost=cost or random.randint(1, 100) * 1000,
+        name=name or fake.name(),
+        description=description or fake.paragraph(nb_sentences=5),
+        expiration_date=expiration_date or fake.date(),
+        size=size or random.choice(SizeType.values),
+        user=user,
+    )
+    return product
+
+
+def create_product_list(n: int = 5, user: User = None) -> [Product]:
+    product_list = []
+    for _ in range(n):
+        product = create_product(user=user)
+        product_list.append(product)
+
+    return product_list
