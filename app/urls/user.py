32fd5e75280a820e84302e4c3645cabc4a2258c5
daehwi <kdh92417@gmail.com>
2024-01-19 14:49:13 +0900

    user url 패턴 수정 : RESTFull API 에 맞게 엔드포인트 이름 수정

--- app/urls/user.py
+++ app/urls/user.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app.views.user import SignupView, LoginView, LogoutView
 
 urlpatterns = [
-    path("user/signup", SignupView.as_view(), name="signup"),
-    path("user/login", LoginView.as_view(), name="login"),
-    path("user/logout", LogoutView.as_view(), name="logout"),
+    path("users/signup", SignupView.as_view(), name="signup"),
+    path("users/login", LoginView.as_view(), name="login"),
+    path("users/logout", LogoutView.as_view(), name="logout"),
 ]
